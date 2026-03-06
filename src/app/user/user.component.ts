38c960bcb3eb7788dab79fab027e31f4b1689f18
Desktop.Admin <desktop.admin@ceteqdev.local>
2026-03-06 15:15:04 +0100

    Selectable users, cleaner component changes

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,10 +1,6 @@
-import { Component, EventEmitter, Input, Output, output} from '@angular/core';
+import { Component, EventEmitter, input, Input, Output, output} from '@angular/core';
 
-interface User {
-  id: string;
-  avatar: string;
-  name: string;
-}
+import { User } from './user.model';
 
 @Component({
   selector: 'app-user',
@@ -16,6 +12,7 @@ interface User {
 export class UserComponent {
 
 @Input({required: true}) user!: User;
+@Input({required: true}) selected!: boolean;
 
   @Output() select = new EventEmitter<string>();
   
